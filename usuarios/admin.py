2b55cf6b3eae23a53b0936896963ee57021141eb
javier <javier.santibanez.93@gmail.com>
2017-06-25 20:16:59 -0400

    Modificando admib.py para administrar Usuarios

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -7,5 +7,8 @@ from django.contrib import admin
 from .models import Usuario
 
 # Register your models here.
-class UsuariorAdmin(admin.ModelAdmin):
+class UsuarioAdmin(admin.ModelAdmin):
 	list_display = ('username', 'email')
+
+
+admin.site.register(Usuario,UsuarioAdmin)
