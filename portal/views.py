665a007b46ec5259eef28727a503ccf85be224a2
Jose <josemtkd@outlook.com>
2017-06-25 21:22:11 -0400

    Slider y Productos destacados template base

--- portal/views.py
+++ portal/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	template_name = "base.html"
+	template_name = "portal/index.html"
 	return render(request, template_name)
 
 
