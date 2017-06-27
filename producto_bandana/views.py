982d6af8a2c23974503134c56eb2785c8303e214
cmardonespino <maadness.20@gmail.com>
2017-06-26 20:32:26 -0400

    eliminando frontend de producto detalle

--- producto_bandana/views.py
+++ producto_bandana/views.py
@@ -11,17 +11,17 @@ def lista_productos(request):
 	data = {}
 	data['object_list'] = Producto.objects.all().order_by('-creado')[:6]
 	data['nueva'] = Producto.objects.filter(nueva = True).order_by('-creado')[:1]
-	template_name = 'portal/producto_bandana.html'
+	template_name = 'producto_bandana/producto_bandana.html'
 	return render(request, template_name, data)
 
 def bandana(request):
-	return render(request, 'portal/producto_bandana.html', {})
+	return render(request, 'producto_bandana/producto_bandana.html', {})
 
 def producto_detalle(request):
-	return render(request, 'portal/producto_detalle.html', {})
+	return render(request, 'producto_bandana/producto_detalle.html', {})
 
 def categoria_alternativa(request):
-	return render(request, 'portal/prod_alternativas.html', {})
+	return render(request, 'producto_bandana/prod_alternativas.html', {})
 
 '''
 def detalle_productos(request, pk):
